3686d2b9c52166b2c83360363efc9b426afd7903
zoytsa <a.baturskiy@gmail.com>
2021-10-13 00:37:16 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+alert('Fight...');
+
+const firstName = 'Sonya';
+let lastName = 'Blade';
+console.log(firstName);
+console.log(lastName);
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+
+const chislo = 555;
+console.log(chislo);
+console.log(typeof chislo);
+
+const infinity = 555 / 0;
+console.log(infinity);
+console.log(typeof infinity);
+
+const NotANumber = 555 * 'Roma';
+console.log(NotANumber);
+console.log(typeof NotANumber);
+
+lastName = 555 * 'Roma';
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = String(lastName);
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = null;
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = 'Roma' + 555;
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = 555 + 'Roma';
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = {} + 'Roma';
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = function myFunction() { };
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = '' + 777;
+console.log(lastName);
+console.log(typeof lastName);
+
+lastName = String(777);
+console.log(lastName);
+console.log(typeof lastName);
+
